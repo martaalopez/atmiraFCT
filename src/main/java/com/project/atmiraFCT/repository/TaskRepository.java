40c3e4fe292ebda95e47179df057826b90ebe3b3
Luis Hidalgo Aguilar <119078933+luishidalgoa@users.noreply.github.com>
2024-02-20 12:21:07 +0100

    Update TaskRepository.java

--- src/main/java/com/project/atmiraFCT/repository/TaskRepository.java
+++ src/main/java/com/project/atmiraFCT/repository/TaskRepository.java
@@ -4,7 +4,6 @@ import com.project.atmiraFCT.model.domain.Colaborator;
 import com.project.atmiraFCT.model.domain.Project;
 import com.project.atmiraFCT.model.domain.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -21,14 +20,8 @@ public interface TaskRepository extends JpaRepository<Task, String> {
 
     Optional<Task> findByIdCode(String idCode);
 
-    @Query("SELECT u FROM Task u WHERE u.tareaPrincipal IS NULL")
-    List<Task> findAllTasks(String startNumber);
 
-
-    @Query("SELECT u FROM Task u WHERE u.tareaPrincipal IS NOT NULL")
-    List<Task> findSubtasksByParentTaskId(String parentTaskId);
-
-    List<Task> findByParentTaskId(String parentTaskId);
-
+   /* @Query("SELECT MAX(CAST(SUBSTRING(t.id_code, LENGTH(t.id_code) - LOCATE('_', REVERSE(t.id_code)) + 2, LENGTH(t.id_code)) AS integer)) FROM Task t WHERE t.task = :parentTaskIdCode")
+    Integer findMaxSubTaskNumber(@Param("parentTaskIdCode") String parentTaskIdCode);*/
 
 }
