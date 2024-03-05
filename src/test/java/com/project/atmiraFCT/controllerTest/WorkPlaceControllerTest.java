25c665487cbe8f87e570e0487a7029de7b9b599b
Luis <luishidalgoa@outlook.es>
2024-03-05 11:05:12 +0100

    LHA- main: deployment

--- src/test/java/com/project/atmiraFCT/controllerTest/WorkPlaceControllerTest.java
+++
@@ -1,4 +0,0 @@
-package com.project.atmiraFCT.controllerTest;
-
-public class WorkPlaceControllerTest {
-}
--- src/test/java/com/project/atmiraFCT/serviceTest/WorkPlaceServiceTest.java
+++
@@ -1,4 +0,0 @@
-package com.project.atmiraFCT.serviceTest;
-
-public class WorkPlaceServiceTest {
-}
